e760713563ae704f8a236cc8ab0f08d07299eee6
wxj <wxj@wxj.com>
2016-08-24 15:33:22 +0800

    Add WebBrowse (CefSharp)

---
+++ Client/ctrl/MyWebBrowse.xaml.cs
@@ -0,0 +1,107 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+
+using CefSharp.Wpf;
+
+namespace TrboX
+{
+    /// <summary>
+    /// MyWebBrowse.xaml 的交互逻辑
+    /// </summary>
+    public partial class MyWebBrowse : UserControl, IRequestHandler
+    {
+        private WebView _view;
+
+        public MyWebBrowse(string url)
+        {
+            InitializeComponent();
+
+            CEF.Initialize(new Settings { LogSeverity = LogSeverity.Disable, PackLoadingDisabled = true });
+
+            BrowserSettings browserSetting = new BrowserSettings { ApplicationCacheDisabled = true, PageCacheDisabled = true };
+
+            _view = new WebView(string.Empty, browserSetting)
+            {
+                Address = url,
+                RequestHandler = this,
+                Background = Brushes.White
+            };
+
+            _view.RegisterJsObject("callbackObj", new CallbackObjectForJs());
+
+            _view.LoadCompleted += _view_LoadCompleted;
+
+            MainGrid.Children.Insert(0, _view);
+        }
+
+        private void _view_LoadCompleted(object sender, LoadCompletedEventArgs url)
+        {
+            Dispatcher.BeginInvoke(new Action(() => 
+            {
+                _view.Visibility = Visibility.Visible;
+                maskLoading.Visibility = Visibility.Collapsed;
+            }));
+        }
+
+        public void View(string url)
+        {
+            if(_view.IsBrowserInitialized)
+            {
+                _view.Visibility = Visibility.Hidden;
+
+                maskLoading.Visibility = Visibility.Visible;
+
+                _view.Load(url);
+            }
+        }
+
+        #region IRequestHandler
+        public bool GetAuthCredentials(IWebBrowser browser, bool isProxy, string host, int port, string realm, string scheme, ref string username, ref string password)
+        {
+            return false;
+        }
+
+        public bool GetDownloadHandler(IWebBrowser browser, string mimeType, string fileName, long contentLength, ref IDownloadHandler handler)
+        {
+            return true;
+        }
+
+        public bool OnBeforeBrowse(IWebBrowser browser, IRequest request, NavigationType naigationvType, bool isRedirect)
+        {
+            return false;
+        }
+
+        public bool OnBeforeResourceLoad(IWebBrowser browser, IRequestResponse requestResponse)
+        {
+            return false;
+        }
+
+        public void OnResourceResponse(IWebBrowser browser, string url, int status, string statusText, string mimeType, WebHeaderCollection headers)
+        {
+            
+        }
+        #endregion
+    }
+
+    public class CallbackObjectForJs
+    {
+        public void showMessage(string msg)
+        {
+            MessageBox.Show(msg);
+        }
+    }
+}
--- Client/win/Main.xaml.cs
+++ Client/win/Main.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Forms.Integration;
 
 namespace TrboX
 {
@@ -18,14 +19,32 @@ namespace TrboX
     /// </summary>
     public partial class Main : MyWindow
     {
+        MyWebBrowse Map = new MyWebBrowse("http://www.baidu.com");
         public Main()
         {
             InitializeComponent();
+            WindowsFormsHost host = new WindowsFormsHost();
         }
 
         private void MyWindow_Closed(object sender, EventArgs e)
         {
             Environment.Exit(0);
+        }
+
+        private void MyWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+           
+
+            
+
+            MyWebGrid.Children.Insert(0,Map);
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Map.View("file:///E:/Home/Projects/TrboX 3.0/Prj/TrboX/Debug/amap/index.html");
         }
     }
 }
