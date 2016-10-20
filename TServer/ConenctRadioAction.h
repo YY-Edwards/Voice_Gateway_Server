eaf41856c949777fb815f78f1842accf3eeafc78
wx <190639442@qq.com>
2016-10-20 11:26:32 +0800

    add connectRadioAction.h

---
+++ TServer/ConenctRadioAction.h
@@ -0,0 +1,36 @@
+#include <string>
+#include <mutex>
+#include "../lib/type.h"
+#include "../lib/rpc/include/BaseConnector.h"
+#include "../lib/rpc/include/RpcJsonParser.h"
+#include"Settings.h"
+
+void connectRadioAction(CRemotePeer* pRemote, const std::string& param, uint64_t callId)
+{
+	static std::mutex lock;
+
+	std::lock_guard<std::mutex> locker(lock);
+	
+	try{
+		Document d;
+		d.Parse(param.c_str());
+		if (d.HasMember("radioIP") && d.HasMember("mnisIP"))
+		{
+			std::string radioIP = d["radioIP"].GetString();
+			std::string mnisIP = d["mnisIP"].GetString();
+			std::map<std::string, std::string> args;
+			args["radioIP"] = radioIP;
+			args["mnisIP"] = mnisIP;
+			std::string strResp = CRpcJsonParser::buildResponse("sucess", callId, 200, "", args);
+			pRemote->sendResponse(strResp.c_str(), strResp.size());
+		}
+	}
+	catch (std::exception e){
+
+	}
+	catch (...)
+	{
+
+	}
+
+}
