43e7727d0a01f461ea0a7b91ad3b114219f6d9d3
wxj <447366284@QQ.com>
2017-05-10 10:25:11 +0800

    Add CServer.cs

---
+++ Client/class/CServer.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.ServiceProcess;
+using System.Configuration.Install;
+
+using System.Collections;
+
+using Microsoft.Win32;
+
+namespace TrboX
+{
+    class CServer
+    {
+        private bool InstallService(string NameService)
+        {
+            bool flag = true;
+            if (!IsServiceIsExisted(NameService))
+            {
+                try
+                {
+                    string location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                    string serviceFileName = location.Substring(0, location.LastIndexOf('\\') + 1) + NameService + ".exe";
+                    InstallmyService(null, serviceFileName);
+                }
+                catch
+                {
+                    flag = false;
+                }
+
+            }
+            return flag;
+        }
+
+
+        private bool UninstallService(string NameService)
+        {
+            bool flag = true;
+            if (IsServiceIsExisted(NameService))
+            {
+                try
+                {
+                    string location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                    string serviceFileName = location.Substring(0, location.LastIndexOf('\\') + 1) + NameService + ".exe";
+                    UnInstallmyService(serviceFileName);
+                }
+                catch
+                {
+                    flag = false;
+                }
+            }
+            return flag;
+        }
+
+        public static bool IsServiceIsExisted(string NameService)
+        {
+            ServiceController[] services = ServiceController.GetServices();
+            foreach (ServiceController s in services)
+            {
+                if (s.ServiceName.ToLower() == NameService.ToLower())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static void InstallmyService(IDictionary stateSaver, string filepath)
+        {
+            AssemblyInstaller AssemblyInstaller1 = new AssemblyInstaller();
+            AssemblyInstaller1.UseNewContext = true;
+            AssemblyInstaller1.Path = filepath;
+            AssemblyInstaller1.Install(stateSaver);
+            AssemblyInstaller1.Commit(stateSaver);
+            AssemblyInstaller1.Dispose();
+        }
+
+        public static void UnInstallmyService(string filepath)
+        {
+            AssemblyInstaller AssemblyInstaller1 = new AssemblyInstaller();
+            AssemblyInstaller1.UseNewContext = true;
+            AssemblyInstaller1.Path = filepath;
+            AssemblyInstaller1.Uninstall(null);
+            AssemblyInstaller1.Dispose();
+        }
+
+        public static bool IsServiceStart(string serviceName)
+        {
+            ServiceController psc = new ServiceController(serviceName);
+            bool bStartStatus = false;
+            try
+            {
+                if (!psc.Status.Equals(ServiceControllerStatus.Stopped))
+                {
+                    bStartStatus = true;
+                }
+
+                return bStartStatus;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public static bool ChangeServiceStartType(int startType, string serviceName)
+        {
+            try
+            {
+                RegistryKey regist = Registry.LocalMachine;
+                RegistryKey sysReg = regist.OpenSubKey("SYSTEM");
+                RegistryKey currentControlSet = sysReg.OpenSubKey("CurrentControlSet");
+                RegistryKey services = currentControlSet.OpenSubKey("Services");
+                RegistryKey servicesName = services.OpenSubKey(serviceName, true);
+                servicesName.SetValue("Start", startType);
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+            return true;
+
+
+        }
+
+        public bool StartService(string serviceName)
+        {
+            bool flag = true;
+            if (IsServiceIsExisted(serviceName))
+            {
+                System.ServiceProcess.ServiceController service = new System.ServiceProcess.ServiceController(serviceName);
+                if (service.Status != System.ServiceProcess.ServiceControllerStatus.Running && service.Status != System.ServiceProcess.ServiceControllerStatus.StartPending)
+                {
+                    service.Start();
+                    for (int i = 0; i < 60; i++)
+                    {
+                        service.Refresh();
+                        System.Threading.Thread.Sleep(1000);
+                        if (service.Status == System.ServiceProcess.ServiceControllerStatus.Running)
+                        {
+                            break;
+                        }
+                        if (i == 59)
+                        {
+                            flag = false;
+                        }
+                    }
+                }
+            }
+            return flag;
+        }
+
+        private bool StopService(string serviceName)
+        {
+            bool flag = true;
+            if (IsServiceIsExisted(serviceName))
+            {
+                System.ServiceProcess.ServiceController service = new System.ServiceProcess.ServiceController(serviceName);
+                if (service.Status == System.ServiceProcess.ServiceControllerStatus.Running)
+                {
+                    service.Stop();
+                    for (int i = 0; i < 60; i++)
+                    {
+                        service.Refresh();
+                        System.Threading.Thread.Sleep(1000);
+                        if (service.Status == System.ServiceProcess.ServiceControllerStatus.Stopped)
+                        {
+                            break;
+                        }
+                        if (i == 59)
+                        {
+                            flag = false;
+                        }
+                    }
+                }
+            }
+            return flag;
+        }
+    }
+}
