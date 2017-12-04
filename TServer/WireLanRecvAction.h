d5ef56e6bda8197b7d1e242d91c9d0c41e8a8bfb
chd <604666734@qq.com>
2017-12-04 17:39:39 +0800

    wlinfo tServer侧过滤;

--- TServer/WireLanRecvAction.h
+++ TServer/WireLanRecvAction.h
@@ -84,22 +84,39 @@ void wlInfoActionHandler(CRemotePeer* pRemote, const std::string& param, uint64_
 
 	std::lock_guard<std::mutex> locker(lock);
 
-	try{
-		std::string callCommand = CRpcJsonParser::mergeCommand("wlInfo", callId, param.c_str());
-		int ret = CBroker::instance()->getWireLanClient()->sendRequest(callCommand.c_str(),
-			callId,
-			pRemote,
-			[&](const char* pResponse, void* data){
-			CRemotePeer* pCommandSender = (CRemotePeer*)data;
-			pCommandSender->sendResponse(pResponse, strlen(pResponse));
-		}, nullptr);
-
-		if (-1 == ret)
+	try
+	{
+		Document d;
+		d.Parse(param.c_str());
+		if (d.HasMember("getType") && d["getType"].IsInt())
 		{
-			// remote error or disconnected
-			std::map<std::string, std::string> args;
-			std::string strResp = CRpcJsonParser::buildResponse("failed", callId, 404, "", ArgumentType());
-			pRemote->sendResponse(strResp.c_str(), strResp.size());
+			int getType = d["getType"].GetInt();
+			if (getType == 4)
+			{
+				if (d.HasMember("SessionId") && d["SessionId"].IsString())
+				{
+					CBroker::instance()->sendSystemStatusToClient(d["SessionId"].GetString(), pRemote, callId);
+				}
+			}
+			else
+			{
+				std::string callCommand = CRpcJsonParser::mergeCommand("wlInfo", callId, param.c_str());
+				int ret = CBroker::instance()->getWireLanClient()->sendRequest(callCommand.c_str(),
+					callId,
+					pRemote,
+					[&](const char* pResponse, void* data){
+					CRemotePeer* pCommandSender = (CRemotePeer*)data;
+					pCommandSender->sendResponse(pResponse, strlen(pResponse));
+				}, nullptr);
+
+				if (-1 == ret)
+				{
+					// remote error or disconnected
+					std::map<std::string, std::string> args;
+					std::string strResp = CRpcJsonParser::buildResponse("failed", callId, 404, "", ArgumentType());
+					pRemote->sendResponse(strResp.c_str(), strResp.size());
+				}
+			}
 		}
 	}
 	catch (std::exception e){
