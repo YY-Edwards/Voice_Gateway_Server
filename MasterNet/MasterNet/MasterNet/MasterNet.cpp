62c8287c7388080f3f96caf2106c3de7b4ce4f0f
Edwards <330713927@qq.com>
2018-01-25 09:59:47 +0800

    test-nochange

--- MasterNet/MasterNet/MasterNet/MasterNet.cpp
+++ MasterNet/MasterNet/MasterNet/MasterNet.cpp
@@ -6,6 +6,20 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	char operat='\0';
+	while ((operat=getchar()) != '0')
+	{
+		switch (operat)
+		{
+		case '1':
+		{
+					//do something by yangyi
+		}
+			break;
+		default:
+			break;
+		}
+	}
 	return 0;
 }
 
